d325860221d11d48ca212e0643fcc5a075a6ea43
matheus lobo <Matheus>
2023-09-24 14:53:36 -0300

    Segundo commit

---
+++ estudos.js
@@ -0,0 +1,21 @@
+function primeiraFuncao(){
+    console.log("Hello World das funções")
+}
+
+primeiraFuncao()
+
+function dizerNome(nome){
+
+    console.log("O nome é: " + nome);
+}
+
+dizerNome("Matheus")
+
+function soma(a, b){
+    return a + b;
+}
+
+
+var somaUm = soma (2,8)
+
+console.log(somaUm)
